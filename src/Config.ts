159ad518c0ecfc7f19af6f3025abc2a1ea735d4d
gaga <dev54188i@outlook.com>
2025-07-11 17:00:22 +0800

    pupm

--- src/Config.ts
+++ src/Config.ts
@@ -1,2 +1,2 @@
-export const TOKEN_MINT_ADDRESS = "EqZ1pYYAiYZnP7fZPqXRtU2eRGCQVLgpNQcsbCnubonk";
+export const TOKEN_MINT_ADDRESS = "BEUV6KjuaQXUVqn1aymssoviXUeZKhfuBRBDej15pump";
 export const RPC_URL = "https://rpc.helius.xyz/?api-key=480a145b-c823-458a-8e4f-a9d35d848d4b";
