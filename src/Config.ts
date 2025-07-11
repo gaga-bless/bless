41590a7209a253506b67009853b76f527a6cee07
gaga <dev54188i@outlook.com>
2025-07-11 17:23:38 +0800

    h

--- src/Config.ts
+++ src/Config.ts
@@ -1,2 +1,2 @@
-export const TOKEN_MINT_ADDRESS = "BEUV6KjuaQXUVqn1aymssoviXUeZKhfuBRBDej15pump";
+export const TOKEN_MINT_ADDRESS = "47FGnsccnjJ9CAtvgxFNv9iC6WpEREvjg9UZeCHCpump";
 export const RPC_URL = "https://rpc.helius.xyz/?api-key=480a145b-c823-458a-8e4f-a9d35d848d4b";
